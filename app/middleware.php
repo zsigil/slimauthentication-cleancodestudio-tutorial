de9765cbab4a163debf61890a9a20ef0c1a940d3
Zsigil <chickpox@freemail.hu>
2021-09-14 10:55:03 +0200

    setting up middleware

---
+++ app/middleware.php
@@ -0,0 +1,16 @@
+<?php
+
+use Slim\App;
+
+return function (App $app){
+    $settings = $app->getContainer()->get('settings');
+
+    $app->addErrorMiddleware(
+        $settings['displayErrorDetails'],
+        $settings['logErrors'],
+        $settings['logErrorDetails'],
+    );
+}
+
+
+?>
--- bootstrap/index.php
+++ bootstrap/index.php
@@ -1,15 +1,26 @@
 <?php
 
+use DI\Container;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+$container = new Container;
+
+$settings = require __DIR__.'/../app/settings.php';
+$settings($container);
+
+Appfactory::setContainer($container);
+
 $app = AppFactory::create();
 
+$middleware = require __DIR__.'/../app/middleware.php';
+$middleware($app);
+
 $app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
+    $response->getBody()->write("Hello baby");
     return $response;
 });
 
